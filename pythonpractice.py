0828cb1eb3f1f4fe2caeaa258fd57845ad3b73b8
Alyssa Yepez <alyssayepez@yahoo.com>
2025-02-18 19:50:42 +0000

    ¨pythonpractice¨

---
+++ pythonpractice.py
@@ -0,0 +1,7 @@
+def give_age():
+    current_age = int(input("What is your current age?"))
+    later_age = current_age + 1
+    print("You will turn " + str(later_age) + " in 2025!")
+
+give_age()
+
